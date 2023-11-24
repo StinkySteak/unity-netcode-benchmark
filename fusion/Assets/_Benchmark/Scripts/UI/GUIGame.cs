5dd988d49bd583fc302a544208542fc7ff06b1cb
Timothy Edrick Burhan <timothy@agate.id>
2023-11-24 15:06:53 +0700

    fusion fix: adjust guiGame based on netcode util changes

--- fusion/Assets/_Benchmark/Scripts/UI/GUIGame.cs
+++ fusion/Assets/_Benchmark/Scripts/UI/GUIGame.cs
@@ -1,39 +1,17 @@
 using Fusion;
 using StinkySteak.NetcodeBenchmark;
 using UnityEngine;
-using UnityEngine.UI;
 
 namespace StinkySteak.FusionBenchmark
 {
     public class GUIGame : BaseGUIGame
     {
-        [Header("Stress Test 1: Move Y")]
-        [SerializeField] private StressTestEssential _test_1;
-
-        [Header("Stress Test 2: Move All Axis")]
-        [SerializeField] private StressTestEssential _test_2;
-
-        [Header("Stress Test 3: Move Wander")]
-        [SerializeField] private StressTestEssential _test_3;
-
-        [System.Serializable]
-        public struct StressTestEssential
-        {
-            public Button ButtonExecute;
-            public int Count;
-            public NetworkObject Prefab;
-        }
-
         private NetworkRunner _runner;
 
         protected override void Initialize()
         {
             base.Initialize();
             _runner = new GameObject("Runner").AddComponent<NetworkRunner>();
-
-            _test_1.ButtonExecute.onClick.AddListener(StartTest_1);
-            _test_2.ButtonExecute.onClick.AddListener(StartTest_2);
-            _test_3.ButtonExecute.onClick.AddListener(StartTest_3);
         }
 
         protected override void StartClient()
@@ -61,19 +39,9 @@ namespace StinkySteak.FusionBenchmark
                 SessionName = "my-session",
             });
         }
-
-        private void StartTest_1()
-            => StartTest(_test_1);
-
-        private void StartTest_2()
-           => StartTest(_test_2);
-
-        private void StartTest_3()
-           => StartTest(_test_3);
-
-        private void StartTest(StressTestEssential stressTest)
+        protected override void StressTest(StressTestEssential stressTest)
         {
-            for (int i = 0; i < stressTest.Count; i++)
+            for (int i = 0; i < stressTest.SpawnCount; i++)
             {
                 _runner.Spawn(stressTest.Prefab);
             }
