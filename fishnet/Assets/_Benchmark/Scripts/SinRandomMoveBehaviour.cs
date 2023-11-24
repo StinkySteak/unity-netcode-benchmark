4e615d9e5cbc1ac946e0f32b064cd4c6762c7edd
Timothy Edrick Burhan <timothy@agate.id>
2023-11-24 14:47:25 +0700

    fishnet feat: added move behaviours

---
+++ fishnet/Assets/_Benchmark/Scripts/SinRandomMoveBehaviour.cs
@@ -0,0 +1,27 @@
+using FishNet.Object;
+using StinkySteak.NetcodeBenchmark;
+using UnityEngine;
+
+namespace StinkySteak.FishnetBenchmark
+{
+    public class SinRandomMoveBehaviour : NetworkBehaviour
+    {
+        [SerializeField] private BehaviourConfig _config;
+        private SinRandomMoveWrapper _wrapper;
+
+        public override void OnStartNetwork()
+        {
+            if (!IsServer) return;
+
+            _config.ApplyConfig(ref _wrapper);
+            _wrapper.NetworkStart();
+            
+            TimeManager.OnTick += OnTick;
+        }
+
+        private void OnTick()
+        {
+            _wrapper.NetworkUpdate(transform);
+        }
+    }
+}
