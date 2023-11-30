5f02a8c3eac1d9de6062f5a784438ef22ff4f2dc
Timothy Edrick Burhan <timothy@agate.id>
2023-11-30 15:47:07 +0700

    mirror fix: do not move object if client

--- mirror/Assets/_Benchmark/Scripts/SineMoveRandomBehaviour.cs
+++ mirror/Assets/_Benchmark/Scripts/SineMoveRandomBehaviour.cs
@@ -11,12 +11,16 @@ namespace StinkySteak.MirrorBenchmark
 
         public override void OnStartServer()
         {
+            if (isClient) return;
+
             _config.ApplyConfig(ref _wrapper);
             _wrapper.NetworkStart(transform);
         }
 
         private void FixedUpdate()
         {
+            if (isClient) return;
+
             _wrapper.NetworkUpdate(transform);
         }
     }
