5e5683f3511e395b01c0bd4474e2474b6243eba9
James Frowen <jamesfrowendev@gmail.com>
2023-12-01 15:21:10 +0000

    benchmark files
    copied from mirror version

---
+++ mirage/Assets/_Benchmark/Scripts/WanderMoveBehaviour.cs
@@ -0,0 +1,32 @@
+using Mirage;
+using StinkySteak.NetcodeBenchmark;
+using UnityEngine;
+
+namespace StinkySteak.MirrorBenchmark
+{ 
+    public class WanderMoveBehaviour : NetworkBehaviour
+    {
+        [SerializeField] private BehaviourConfig _config;
+        private WanderMoveWrapper _wrapper;
+
+        private void Awake()
+        {
+            Identity.OnStartServer.AddListener(OnStartServer);
+        }
+
+        public void OnStartServer()
+        {
+            if (IsClient) return;
+
+            _config.ApplyConfig(ref _wrapper);
+            _wrapper.NetworkStart(transform);
+        }
+
+        private void FixedUpdate()
+        {
+            if (IsClient) return;
+
+            _wrapper.NetworkUpdate(transform);
+        }
+    }
+}
