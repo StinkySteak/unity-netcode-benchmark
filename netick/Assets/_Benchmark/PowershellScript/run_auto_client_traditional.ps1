74f85478370ef2e89e20e500f2c14029b430c8a8
StinkySteak <slimesteve17@gmail.com>
2024-03-17 15:42:41 +0700

    netick powershell: add run auto client traditional

---
+++ netick/Assets/_Benchmark/PowershellScript/run_auto_client_traditional.ps1
@@ -0,0 +1,17 @@
+# Prompt the user for the client count
+$clientCount = Read-Host "Enter the number of clients"
+$serverIP = Read-Host "Enter the Server IP Address (default is 127.0.0.1)"
+
+if($serverIP -eq "") {
+    $serverIP = "127.0.0.1"
+}
+
+# Validate and convert the input to an integer
+while (-not ($clientCount -as [int])) {
+    Write-Host "Invalid input. Please enter a valid integer."
+    $clientCount = Read-Host "Enter the number of clients"
+}
+
+for ($i = 1; $i -le $clientCount; $i++) {
+    Start-Process -FilePath "netick.exe" -ArgumentList "-autoclient", "-clientcount", "1", "-serverip", "$serverIP"
+}
