d316e3084ab3d46d3f1edcaa92187ec28f1c18b2
StinkySteak <slimesteve17@gmail.com>
2024-03-18 14:40:10 +0700

    mirror powershell: add powershell scripts

---
+++ mirror/Assets/_Benchmark/PowershellScript/run_autoserver.ps1
@@ -0,0 +1,8 @@
+$testNumber = Read-Host "Enter the test number (1, 2, 3)"
+
+while (-not ($testNumber -as [int])) {
+    Write-Host "Invalid input. Please enter a valid integer."
+    $testNumber = Read-Host "Enter the test number (1, 2, 3)"
+}
+
+Start-Process -FilePath "mirror.exe" -ArgumentList "-autoserver", "-batchmode", "-nographics","-test$testNumber"
