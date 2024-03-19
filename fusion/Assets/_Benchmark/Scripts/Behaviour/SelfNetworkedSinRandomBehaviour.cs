2fa7f4beb4dacc0336643daf9d52ea3f304e0efc
StinkySteak <slimesteve17@gmail.com>
2024-03-19 16:41:01 +0700

    fusion feat: self networked sin random move behaviour

---
+++ fusion/Assets/_Benchmark/Scripts/Behaviour/SelfNetworkedSinRandomBehaviour.cs
@@ -0,0 +1,35 @@
+using Fusion;
+using StinkySteak.NetcodeBenchmark;
+using UnityEngine;
+
+namespace StinkySteak.FusionBenchmark
+{
+    public class SelfNetworkedSinRandomBehaviour : NetworkBehaviour
+    {
+        [Networked] private Vector3 _serverPosition { get; set; }
+
+        [SerializeField] private BehaviourConfig _config;
+        private SinRandomMoveWrapper _wrapper;
+
+
+        public override void Spawned()
+        {
+            if (!Object.HasStateAuthority) return;
+
+            _config.ApplyConfig(ref _wrapper);
+            _wrapper.NetworkStart(transform);
+        }
+
+        public override void FixedUpdateNetwork()
+        {
+            if (!Object.HasStateAuthority)
+            {
+                transform.position = _serverPosition;
+                return;
+            }
+
+            _wrapper.NetworkUpdate(transform);
+            _serverPosition = transform.position;
+        }
+    }
+}
