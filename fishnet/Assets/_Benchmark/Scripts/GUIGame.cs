586b967e0648bbe018a704a013e851986ace4de1
StinkySteak <slimesteve17@gmail.com>
2024-03-19 16:37:38 +0700

    fishnet feat: stress test 4
    several refactor
    guiGame now is a prefab

--- fishnet/Assets/_Benchmark/Scripts/GUIGame.cs
+++ fishnet/Assets/_Benchmark/Scripts/GUIGame.cs
@@ -1,33 +1,22 @@
 using FishNet.Managing;
 using StinkySteak.NetcodeBenchmark;
 using StinkySteak.NetcodeBenchmark.Util;
-using System.IO;
-using System.Text;
 using UnityEngine;
 
 namespace StinkySteak.FishnetBenchmark
 {
-    public class GUIGame : BaseGUIGame
+    public class GUIGame : BaseGUIGame, INetcodeBenchmarkRunner
     {
         [SerializeField] private NetworkManager _networkManagerPrefab;
         private NetworkManager _networkManager;
         [SerializeField] private FrametimeCounter _counter;
+        private FrametimeLogger _frametimeLogger;
 
-        private const int STRING_BUILDER_CAPACITY = 100;
-        private StringBuilder _stringBuilder = new(STRING_BUILDER_CAPACITY);
-
-        private const string ARGS_AUTO_CLIENT = "-autoclient";
-        private const string ARGS_AUTO_SERVER = "-autoserver";
-        private const string ARGS_CLIENT_COUNT = "-clientcount";
-        private const string ARGS_SERVER_IP = "-serverip";
-
-        private SimulationTimer.SimulationTimer _timerServerLog;
-        private float _intervalServerLog = 1f;
-        private string _filePath;
 
         protected override void MonoStart()
         {
-            _filePath = Application.persistentDataPath + "/FishnetServerOutput.txt";
+            _frametimeLogger = new FrametimeLogger();
+            _frametimeLogger.Initialize(this);
 
             RunAutoServer();
             RunAutoClient();
@@ -35,7 +24,7 @@ namespace StinkySteak.FishnetBenchmark
 
         private void RunAutoServer()
         {
-            bool isAutoServer = HeadlessUtils.HasArg(ARGS_AUTO_SERVER);
+            bool isAutoServer = HeadlessUtils.HasArg(HeadlessArguments.AUTO_SERVER);
 
             if (!isAutoServer) return;
 
@@ -48,16 +37,16 @@ namespace StinkySteak.FishnetBenchmark
             int clientCount = 0;
             string serverIp = string.Empty;
 
-            bool isAutoClient = HeadlessUtils.HasArg(ARGS_AUTO_CLIENT);
+            bool isAutoClient = HeadlessUtils.HasArg(HeadlessArguments.AUTO_CLIENT);
 
             if (!isAutoClient) return;
 
-            if (HeadlessUtils.TryGetArg(ARGS_CLIENT_COUNT, out string argsClientCount))
+            if (HeadlessUtils.TryGetArg(HeadlessArguments.CLIENT_COUNT, out string argsClientCount))
             {
                 clientCount = int.Parse(argsClientCount);
             }
 
-            if (HeadlessUtils.TryGetArg(ARGS_SERVER_IP, out string argsServerIp))
+            if (HeadlessUtils.TryGetArg(HeadlessArguments.SERVER_IP, out string argsServerIp))
             {
                 serverIp = argsServerIp;
             }
@@ -98,7 +87,7 @@ namespace StinkySteak.FishnetBenchmark
             _textLatency.SetText("Latency: {0}ms", _networkManager.TimeManager.RoundTripTime);
         }
 
-        protected override void MonoLateUpdate()
+        protected override void MonoUpdate()
         {
             if (!HeadlessUtils.IsHeadlessMode()) return;
 
@@ -119,17 +108,17 @@ namespace StinkySteak.FishnetBenchmark
 
         private void PrintAverageFrameTime()
         {
-            if (!_timerServerLog.IsExpiredOrNotRunning()) return;
-            
-            int connectedClients = _networkManager.ServerManager.Clients.Count;
-            float avgFrameTime = _counter.GetAvgFrameTime();
+            _frametimeLogger.MonoUpdate();
+        }
 
-            _stringBuilder.Clear();
-            _stringBuilder.AppendFormat("Average FrameTime: {0}ms. Connected Clients: {1}\n", avgFrameTime, connectedClients);
+        public int GetConnectedClients()
+        {
+            return _networkManager.ServerManager.Clients.Count;
+        }
 
-            File.AppendAllText(_filePath, _stringBuilder.ToString());
-
-            _timerServerLog = SimulationTimer.SimulationTimer.CreateFromSeconds(_intervalServerLog);
+        public float GetAverageFrameTime()
+        {
+            return _counter.GetAvgFrameTime();
         }
     }
 }
